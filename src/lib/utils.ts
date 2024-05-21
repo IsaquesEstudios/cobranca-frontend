d664d559ea9b492e397428346a0df764e97fa811
matteus isaque <matteus.isaque28@gmail.com>
2024-05-21 11:40:11 -0300

    alt

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 import nookies from "nookies";
 
 const development = "http://localhost:3333";
-const production = "https://ia.isaquesestudios.com"
+const production = "https://main-form.herokuapp.com/"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
